e41d44f144eacb5e43715eaae16b0d130c13425a
Dave <oblivion.davecave@gmail.com>
2017-02-25 00:27:45 -0300

    Adição de testes do sonar e resultados
    - Testes de leitura do sonar a várias distâncias e diferentes taxas de atualização

---
+++ Tests/Sonar/measurements_test.py
@@ -0,0 +1,52 @@
+import RPi.GPIO as GPIO
+import time
+import sys
+GPIO.setmode(GPIO.BCM)
+
+TRIG = 26
+ECHO = 19
+
+print "Distance Measurement In Progress"
+
+GPIO.setup(TRIG,GPIO.OUT)
+GPIO.setup(ECHO,GPIO.IN)
+
+GPIO.output(TRIG, False)
+print "Waiting For Sensor To Settle"
+time.sleep(2)
+
+msr = 20
+frqs = [20.0, 30.0, 50.0, 100.0, 1000.0, 10000.0]
+
+print msr, "Measurements"
+print "Frequency:", frqs, "Hz"
+print "Distances (cm)"
+
+for frq in frqs:
+
+    print "\n\n\n\n"
+
+    for j in range(20):
+
+        GPIO.output(TRIG, True)
+        time.sleep(0.00001)
+        GPIO.output(TRIG, False)
+
+        while GPIO.input(ECHO)==0:
+          pulse_start = time.time()
+
+        while GPIO.input(ECHO)==1 or time.time() - pulse_start > 1:
+          pulse_end = time.time()
+
+        if pulse_end - pulse_start < 1:
+            pulse_duration = pulse_end - pulse_start
+            distance = pulse_duration * 17150
+            distance = round(distance, 2)
+        else:
+            distance = -1
+
+        print distance
+
+        time.sleep(1/frq)
+
+GPIO.cleanup()
